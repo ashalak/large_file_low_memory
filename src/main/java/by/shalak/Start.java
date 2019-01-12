8a01b1ce5e262773573bb75f6581f583c85a3ef1
Alexey Shalak <aliaksei.shalak@rednavis.com>
2019-01-13 00:14:52 +0300

    Add Maven

---
+++ src/main/java/by/shalak/Start.java
@@ -0,0 +1,21 @@
+package by.shalak;
+
+/**
+ * The problems is:
+ * Given with a line separated text file of integers ranging anywhere from Integer.MIN to Integer.MAX of size 1024MB,
+ * the program should be able to produce line separated text file which has the sorted content of the input file.
+ *
+ * Following preconditions:
+ * the program should be able to run with a memory constraint of 100MB i.e. the -Xmx100m.
+ * the file can have duplicate integers.
+ * the text in the file has only integers which are line separated and no other characters.
+ *
+ * @author Alexey Shalak
+ */
+public class Start {
+	public static void main(String[] args) {
+		System.out.println("Hello Main!");
+	}
+
+}
+
---
+++ src/test/java/by/shalak/StartTest.java
@@ -0,0 +1,17 @@
+package by.shalak;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Start.
+ */
+public class StartTest {
+
+	/**
+	 * Test
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		System.out.println("Hello Test!");
+	}
+}
