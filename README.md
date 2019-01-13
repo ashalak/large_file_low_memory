8088cb37827a9c0456dc90c5dcfb1ec2685e7eae
Alexey Shalak <aliaksei.shalak@rednavis.com>
2019-01-13 18:29:31 +0300

    Basic logic. Part 1.

--- README.md
+++ README.md
@@ -4,17 +4,19 @@ Given with a line separated text file of integers ranging anywhere from Integer.
 the program should be able to produce line separated text file which has the sorted content of the input file.
 
 ## Following preconditions:
- * the program should be able to run with a memory constraint of 100MB,i.e. the -Xmx100m.
+ * the program should be able to run with a memory constraint of 100MB, i.e. the -Xmx100m.
  * the file can have duplicate integers.
  * the text in the file has only integers which are line separated and no other characters.
 
 ## Run
 
-By using "mvn test" from project command line.
+For run only tests: use "mvn clean test" in command line from project root directory.
+For run program: use "mvn clean package exec:java" in command line from project root directory.
 
 ## Information
 
 Memory constraint of 100MB sets in .mvn/jvm.config, for tests in maven-surefire-plugin settings.
+Both input_data_file.txt and output_data_file.txt files are created in project root directory.
 
 ## Author
 Alexey Shalak 
--- src/main/java/by/shalak/Start.java
+++ src/main/java/by/shalak/Start.java
@@ -1,53 +1,151 @@
 package by.shalak;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /**
  * @author Alexey Shalak
  */
 public class Start {
 	private static Path inputDataFile = Paths.get("input_data_file.txt");
-	private static final int INPUT_DATA_FILE_SIZE_MB = 30;
+	private static Path outputDataFile = Paths.get("output_data_file.txt");
 
-	public static void main(String[] args) {
-		try {
-			createInputFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static int inputDataFileSize = 1024 * 1024 * 15; // size of the inputDataFile
+	private static int maxMemorySize = (int) Runtime.getRuntime().maxMemory(); // size of the max memory
+	private static int tempFilesCount = (inputDataFileSize / maxMemorySize + 1) * 10;
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("inputDataFileSize - " + inputDataFileSize / 1024 / 1024 + " MB");
+		System.out.println("maxMemorySize - " + maxMemorySize / 1024 / 1024 + " MB");
+		System.out.println("tempFilesCount - " + tempFilesCount);
+
+		createInputDataFile();
+		List<File> tempFiles = createTempFiles();
+		List<File> sortedTempFiles = sortTempFiles(tempFiles);
+		createOutPutDataFile(sortedTempFiles);
 	}
 
-	/**
-	 * Creation of line separated text file of integers ranging anywhere from Integer.MIN to Integer.MAX of size 1024MB
-	 */
-	private static void createInputFile() throws IOException {
+	private static void createInputDataFile() throws IOException {
 		if (Files.notExists(inputDataFile)) {
 			Files.createFile(inputDataFile);
+			System.out.println("Creation of input_data_file.txt...");
 
-			try (FileWriter writer = new FileWriter(inputDataFile.toFile())) {
+			try (PrintWriter printWriter = new PrintWriter(new FileWriter(inputDataFile.getFileName().toString()))) {
 
-				System.out.println("Creation of input_data_file.txt...");
-
-				while (inputDataFile.toFile().length() < Math.pow(1024, 2) * INPUT_DATA_FILE_SIZE_MB) {
-					writer.write(Integer.toString(getRandomInt()));
-					writer.write(System.lineSeparator());
-					writer.flush();
+				while (inputDataFile.toFile().length() < inputDataFileSize) {
+					printWriter.println((int) ((long) Integer.MIN_VALUE + Math.random() * ((long) Integer.MAX_VALUE - Integer.MIN_VALUE + 1)));
 				}
 			}
 		}
 	}
 
 	/**
-	 * Getting of random int value from Integer.MIN_VALUE to Integer.MAX_VALUE
-	 *
-	 * @return int
+	 * Split the large file into sorted temp files
 	 */
-	private static int getRandomInt() {
-		return (int) ((long) Integer.MIN_VALUE + Math.random() * ((long) Integer.MAX_VALUE - Integer.MIN_VALUE + 1));
+	private static List<File> createTempFiles() throws IOException {
+		List<File> files = new ArrayList<>();
+		long tempFileSize = inputDataFile.toFile().length() / tempFilesCount;
+
+		System.out.println("Creation of temp files...");
+
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputDataFile.toFile()))) {
+			int fileCounter = 1;
+			for (int i = 1; i <= tempFilesCount; i++) {
+
+				File file = new File("temp-file-" + fileCounter + ".txt");
+				try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+
+					String line;
+					while ((line = bufferedReader.readLine()) != null && file.length() <= tempFileSize) {
+						bufferedWriter.write(line);
+						bufferedWriter.write(System.lineSeparator());
+						bufferedWriter.flush();
+					}
+
+					files.add(file);
+					fileCounter++;
+				}
+			}
+		}
+
+		return files;
+	}
+
+	/**
+	 * Sort temp file's data
+	 */
+	private static List<File> sortTempFiles(List<File> tempFiles) throws IOException {
+		System.out.println("Sorting of temp files...");
+
+		for (File tempFile : tempFiles) {
+			List<String> lines = Files.lines(tempFile.toPath()).sorted(Comparator.comparing(Integer::valueOf))
+							.collect(Collectors.toList());
+
+			try (BufferedWriter newBufferedWriter = Files.newBufferedWriter(tempFile.toPath())) {
+				for (String line : lines) {
+					newBufferedWriter.write(line);
+					newBufferedWriter.write(System.lineSeparator());
+					newBufferedWriter.flush();
+				}
+			}
+		}
+
+		return tempFiles;
+	}
+
+	/**
+	 * Merge sorted temp files into a sorted outPutDataFile
+	 */
+	private static void createOutPutDataFile(List<File> sortedTempFiles) throws IOException {
+		System.out.println("Creation of output_data_file.txt...");
+
+		List<BufferedReader> bufferedReaders = new ArrayList<>();
+		TreeMap<Integer, BufferedReader> map = new TreeMap<>();
+
+		try (PrintWriter printWriter = new PrintWriter(new FileWriter(outputDataFile.getFileName().toString()))) {
+			for (File file : sortedTempFiles) {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+				bufferedReaders.add(bufferedReader);
+				String line = bufferedReader.readLine();
+				map.put(Integer.valueOf(line), bufferedReader);
+			}
+
+			while (!map.isEmpty()) {
+				Map.Entry<Integer, BufferedReader> nextToGo = map.pollFirstEntry();
+
+				printWriter.println(nextToGo.getKey());
+
+				String line;
+				if ((line = nextToGo.getValue().readLine()) != null) {
+					map.put(Integer.valueOf(line), nextToGo.getValue());
+				}
+			}
+
+		} finally {
+			if (bufferedReaders.size() > 0) {
+				for (BufferedReader br : bufferedReaders) {
+					br.close();
+				}
+			}
+
+			for (File file : sortedTempFiles) {
+				//file.delete();
+			}
+		}
 	}
 }
 
